167bbee047e591bc1c4deb31cf300dd595b754b0
Dave Bushnell <dybushnell@gmail.com>
2019-05-23 01:14:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ function appendKitten(name){
   var newKittens = kittens.concat(name)
   return newKittens
 }
+
+function prependKitten(name){
+  var newKittens = [name,...kittens]
+  return newKittens
+}
